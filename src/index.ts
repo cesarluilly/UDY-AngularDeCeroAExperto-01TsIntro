8a30d7bf5e5b2af6cb551fe0646385064517b009
Cesar Luilly Garcia Aguazul <cesarluilly.aicrag@hotmail.com>
2021-07-04 07:29:51 -0500

    Video 22 - Clases basicas

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,21 @@
     //              //Inician comentarios en 21 y terminan en  *****************
     //              //  terminan en 81.
     ===== Código de TypeScript =====
-    ===== Video  21 - Importaciones y exportaciones.  =====
+    ===== Video  22 - Clases basicas.  =====
 */
 
+class Heroe {
+    alterEgo: string;
+    edad: number;
+    nombreReal: number;
 
+    imprimirEgo(){
+        return this.alterEgo + ' ' + this.nombreReal;
+    }
+}
+
+const iroman = new Heroe();
+console.log(iroman);
 
 
 
